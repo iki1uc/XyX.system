bebea9a6e9d2ec2d594d3abbfc857dea7b868e77
iki1uc <entertrainer@web.de>
2026-09-02 02:31:41 +0200

    Refactor computeAxes to simplify achsenwert calculation

--- axes.js
+++ axes.js
@@ -1,13 +1,18 @@
 // 9hoch9 / axes.js
 
 export function computeAxes(Phi, phi, phi2, phiinfty) {
+    const achsenwert = (Phi + phi + phi2 + phiinfty) / 4;
+
     return {
+        x: Phi * 0.5 + phi * 0.3 + phi2 * 0.2,
+        y: phi * 0.4 + phi2 * 0.4 + phiinfty * 0.2,
+        z: achsenwert,
         Phi,
         phi,
         phi2,
         phiinfty,
         bewegung: Phi * phi,
         stabilitaet: phi2 - phi,
-        achsenwert: (Phi + phi + phi2 + phiinfty) / 4
+        achsenwert
     };
 }
